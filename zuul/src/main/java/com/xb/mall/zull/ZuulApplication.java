c3416febb22c2f5267d830a727ea7db573910af5
baixiaozheng <baixiaozheng@163.com>
2020-06-09 19:18:25 +0800

    添加路由网关Zuul

---
+++ zuul/src/main/java/com/xb/mall/zull/ZuulApplication.java
@@ -0,0 +1,17 @@
+package com.xb.mall.zull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author baixiaozheng
+ * @date 2020/6/9 17:14
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+}
